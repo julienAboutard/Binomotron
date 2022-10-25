6d39613e781ecd0d8c5eea21c4ca3996ffceeecd
Julien Aboutard <julien.aboutard@gmail.com>
2022-10-25 14:06:55 +0200

    Ajout version Binomotron

--- test.py
+++ test.py
@@ -1,20 +1,34 @@
 from random import shuffle
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="user",
+  password="1234",
+  database="alternants"
+)
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM students")
+myresult = mycursor.fetchall()
+
+# print(myresult)
 
 
-prenom = ["Julien","Nawel","Vincent","Djamila","Florian","Tom","Christelle","Wahid","Audrey","Dorine","Marwin","Alice","Hayel"]
-nom = ["Aboutard","Ouarti","Boettcher","Chabane","Berthelot","Bockaj","Wittmann","Ameur","Costes","Paris","Launay","Lafon","Bendib"]
 
-def liste_random(prenom, nom):
-    liste = []
+def liste_random(listdb):
+    # liste = []
     rep=[]
-    for i,j in zip(nom,prenom):
-        liste.append((i,j))
-    while(len(liste)>3) :
-        shuffle(liste)
-        rep.append((liste.pop(0), liste.pop(1)))
+    # for i in listdb:
+    #     liste.append(i)
+    while(len(listdb)>3) :
+        shuffle(listdb)
+        rep.append((listdb.pop(0), listdb.pop(1)))
+    if len(listdb)==3 :
+        rep.append((listdb[0], listdb[1], listdb[2]))
     return rep
-liste = liste_random(prenom, nom)
-print(liste)
+liste = liste_random(myresult)
+for x in liste :
+    print(x)
 
 # def extraction(liste):
 #     a = liste[0]
