9a479209ccd5af62ddf2226197a5f5ac510de530
Hayel <hayelben@hotmail.fr>
2022-10-25 14:30:04 +0200

    Modifications algo

--- binomotron.py
+++ binomotron.py
@@ -1,38 +1,27 @@
-from random import shuffle
-import mysql.connector
+from random import choices, shuffle,choice
+import mariadb
 
-mydb = mysql.connector.connect(
+mydb = mariadb.connect(
   host="localhost",
   user="user",
   password="1234",
-  database="alternants"
+  database="alternants",
 )
 mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM students")
 myresult = mycursor.fetchall()
 
-# print(myresult)
-
-
-
 def liste_random(listdb):
-    # liste = []
     rep=[]
-    # for i in listdb:
-    #     liste.append(i)
     while(len(listdb)>3) :
-        shuffle(listdb)
-        rep.append((listdb.pop(0), listdb.pop(1)))
+        b1=choice(listdb)
+        listdb.remove(b1)
+        b2=choice(listdb)
+        listdb.remove(b2)
+        rep.append((b1,b2))
     if len(listdb)==3 :
         rep.append((listdb[0], listdb[1], listdb[2]))
     return rep
 liste = liste_random(myresult)
 for x in liste :
     print(x)
-
-# def extraction(liste):
-#     a = liste[0]
-#     return groupe
-# print(liste)
-# groupe = extraction(liste)
-# print(groupe)
