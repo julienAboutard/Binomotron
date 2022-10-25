b1a791902cc33d8ddc01c4aef06f1aafbf6b87ef
Hayel <hayelben@hotmail.fr>
2022-10-25 15:01:14 +0200

    Algo finit

--- binomotron.py
+++ binomotron.py
@@ -23,5 +23,16 @@ def liste_random(listdb):
         rep.append((listdb[0], listdb[1], listdb[2]))
     return rep
 liste = liste_random(myresult)
-for x in liste :
-    print(x)
+# for x in liste :
+#     print(x)
+
+
+
+def afficher_groupe(liste):
+    for a in liste :
+        if len(a)==3:
+            print (a[0], ",", a[1], 'et', a[2])
+        else : 
+            print (a[0], "et", a[1])
+        
+afficher_groupe(liste)
